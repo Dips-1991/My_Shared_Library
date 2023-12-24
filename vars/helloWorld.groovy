8876edf3485064aae1585e3d9768244f04bb10a3
Deepak Patil <patildipak0791@gmail.com>
2023-12-24 09:52:07 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
 
 def call()
 {
-   echo "I got my dream job as a DevOps Engineer with my dream salary of 2 Lakh per month with a dream company with amazing facilities...Thank you...Thank you...Thank you so much universe.(*-*)"
+   echo "I got my dream job as a DevOps Engineer with my dream salary of 2 Lakh per month with a dream company with amazing facilities...Thank you...Thank you...Thank you so much universe (*-*)...Now my life is so fantastic."
 }
